c9d3ced983c9f0610654c11dd23c670cfab76baa
UjasBaravaliya <114396028+UjasBaravaliya@users.noreply.github.com>
2024-11-19 17:05:29 +0530

    19-11-2024

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -1,2 +1,8 @@
 const orderModel = require('../models/orderModel');
+const productModel = require("../models/productModel");
+const ErrorHandler = require("../utils/errorhandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
+const createOrder = catchAsyncErrors(async (req, res, next) => {
+  
+})
